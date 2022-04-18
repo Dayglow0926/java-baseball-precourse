966611a9a57017ff59140f612c7e8bb4d6c6a73e
dayglow0926 <dayglow0926@gmail.com>
2022-04-18 19:48:19 +0900

    프로그램 구현 내용 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import play.BaseBallGameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseBallGameController baseballGame = new BaseBallGameController();
+        int startNumber = 0;
+        do{
+            startNumber = baseballGame.start();
+        }while (startNumber==1);
     }
 }
